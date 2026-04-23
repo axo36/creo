0abf38693edffaa5cecb08e3a37a6ec66f37a25b
axo36 <ilanrenon2@gmail.com>
2026-04-23 21:22:09 +0200

    Delete client/admin/js directory

--- client/admin/js/supabase.js
+++
@@ -1,7 +0,0 @@
-/* ══ supabase.js — connexion Supabase ══ */
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-export const supabase = createClient(
-  "https://mpnfvrizbluhhjcfzztc.supabase.co",
-  "sb_publishable_PMOkki7SEbuv11glUpmNTQ_7KTnMrEr"
-);
